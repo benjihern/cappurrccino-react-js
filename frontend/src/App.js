5fbc3cb0dd97956addc5c1223e5d0c6c75bf9bef
Sebastian Infante Murguia <6dinformaticasinfante@gmail.com>
2024-03-24 19:58:49 -0400

    changse to routes so that it works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AuthProvider, useAuth } from "./pages/Login/contexts/authContext/";
-import { useRoutes, Navigate } from "react-router-dom";
+import { useRoutes, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/components/auth/login/login";
 import Register from "./pages/Login/components/auth/register/register";
 import Header from "./pages/Login/components/header/header";
@@ -10,35 +10,22 @@ import MenuPage from "./pages/Menu/MenuPage";
 function App() {
   const { currentUser } = useAuth();
 
-  const routesArray = [
-    {
-      path: "*",
-      element: <Navigate to="/login" />,
-    },
-    {
-      path: "/login",
-      element: currentUser ? <Navigate to="/menu" /> : <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/home",
-      element: <Home />,
-    },
-    {
-      path: "/menu",
-      element: currentUser ? <MenuPage /> : <Navigate to="/login" />,
-    },
-  ];
-
-  const routesElement = useRoutes(routesArray);
-
   return (
     <AuthProvider>
       <Header />
-      <div className="w-full h-screen flex flex-col">{routesElement}</div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={currentUser ? <Navigate to="/menu" /> : <Login />}
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/menu"
+          element={currentUser ? <MenuPage /> : <Navigate to="/login" />}
+        />
+      </Routes>
     </AuthProvider>
   );
 }
--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MenuPage from "./pages/Menu/MenuPage";
 import Login from "./pages/Login/components/auth/login/login";
-// import Login from "./pages/Login/components/auth/login";
-// import Register from "./pages/Login/components/auth/register";
+import Register from "./pages/Login/components/auth/register";
 
 export default function AppRoutes() {
   return (
